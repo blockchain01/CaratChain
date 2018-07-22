597f1e74af87aecb86af8bb4c6e44b88677f6702
blockchain01 <41298195+blockchain01@users.noreply.github.com>
2018-07-22 12:45:00 +0800

    Update CC-Rules.sol

--- solidity/Test/CC-Rules.sol
+++ solidity/Test/CC-Rules.sol
@@ -1,77 +1,187 @@
-//版本聲明
 pragma solidity ^0.4.24;
 
 
 
-/////////////////////////////////////////////////////////////////設定合約持有人
-contract SetOwner{ 
-//合約持有人地址
-    address public owner;
-//購買721價格 以SZABO計算,1000000SZABO=1Ether  
-    uint public Price = 3000 szabo; 
+import "./CC-Rules.sol";  
 
 
-  
-    function SetOwner() { 
-//初始發設定合約持有人  
-        owner = msg.sender;
-        
-        
+//InterfaceERC-20合約
+//interface InterfaceTocontract {
+//  function balance(address _myAddress) public view returns (uint);
+//  function burn(uint256 _value) public returns (bool success);
+//}
+
+//外部使用受權
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
+
+
+
+
+//本合約
+contract caratchainTokenERC721 is BasicRules {
+
+
+//記錄舊合約地址  
+//  address InterfaceToAdd = OutAdd0x0...;
+//  InterfaceTocontract ToThisContract = InterfaceTocontract(InterfaceToAdd);
+
+///////////////////////////////////////////////////////////////////////// event
+
+//轉帳時通知區塊
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+//受權時通知區塊
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+//燒毀時通知區塊
+    event Burn(address indexed from, uint256 value);
+    
+////////////////////////////////////////////////////////////////////////基本資料
+
+//名,符號,小數,總量
+    string public name = "Carat Chain" ;
+    string public symbol = "CRT";
+    uint8 public decimals = 0;
+    uint256 public totalSupply;
+    
+////////////////////////////////////////////////////////////////////////個人資料
+
+//戶口
+    mapping (address => uint256) public balanceOf;
+
+//獨立網址
+    mapping (address => string) public CCindex;
+
+//購買費用
+    mapping (address => uint256) public ExchangeRate;
+    
+//受權額度  
+    mapping (address => mapping (address => uint256)) public allowance;
+
+
+
+////////////////////////////////////////////////////////////////////////合約功能
+
+
+//受權額度給其他人
+    function approve(
+        address _spender, //被受權者戶口
+        uint256 _value    //受權額
+    ) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;  //記錄受權額
+        emit Approval(msg.sender, _spender, _value);  //受權時通知區塊
+        return true;   //成功通知
     }
-}
+
+//在其他DAPP顯示
+    function approveAndCall(
+        address _spender,  //被受權者戶口
+        uint256 _value,    //受權額
+        bytes _extraData   //受權者留言
+    ) public
+        returns (bool success) {
+//被受權者戶口可在其他DAPP使用
+        tokenRecipient spender = tokenRecipient(_spender);
+//如果有被受權
+        if (approve(_spender, _value)) {
+//將資料記錄在被受權者戶口(受權者'我',受權額,本合約地址,受權者留言)
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true; //成功通知
+        }
+    }
 
 
-/////////////////////////////////////////////////////////////////////////規則
-contract BasicRules is SetOwner{
 
-//驗證持有人,不是持有人則消耗GAS但取消所有動作
- modifier JustOwner() {assert(msg.sender == owner);  _;}
+
+//轉帳
+    function transfer(
+        address _to,      //收帳人
+        uint256 _value    //轉帳額
+    ) public returns (bool success) {
+//轉帳時的數據進入內部功能
+        _transfer(msg.sender, _to, _value);
+        return true;      //成功通知
+    }
+
+//受權額度轉帳
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+//轉帳額少於受權額度
+        require(_value <= allowance[_from][msg.sender]);
+//受權轉帳額度減轉帳額
+        allowance[_from][msg.sender] -= _value;
+//轉帳時的數據進入內部功能
+        _transfer(_from, _to, _value);
+        return true;     //成功通知
+    }
+
+
+//銷毀TOKEN
+    function burn(uint256 _value) public returns (bool success) {
+//銷毀量少於戶口現有額
+        require(balanceOf[msg.sender] >= _value); 
+//在我的戶口中銷毀
+        balanceOf[msg.sender] -= _value;  
+//在總量中減少
+        totalSupply -= _value; 
+//燒毀時通知區塊
+        emit Burn(msg.sender, _value);
+        return true;  //成功通知
+    }
     
-//新Owner不能是0,是0則取消所有動作
- modifier AddRrule(address _T) {require(address(0x0) != _T);  _;}
- 
-//新匯率必須大於0||少於0則取消所有動作
- modifier ExchangeRateRrule(uint _E) {require(0 < _E);  _;}
-}
+
+
+//購買721代幣
+  function making721(string index) external payable{
+    //入帳給owner  
+    owner.transfer(msg.value);
+    //驗證價格  
+    require(msg.value > Price || msg.value == Price);  
+    //進入內部功能-新增代幣
+    NewToken(msg.sender,index,msg.value);
+  }
 
 
 
-/////////////////////////////////////////////////////////////持有人修改合約資料
-contract Admin is BasicRules {
 
 
-//修改-購買721價格 以SZABO計算,1000000SZABO=1Ether     
-function ChangePrice(uint InputPrice) 
-//驗證持有人//價格必須大於0
-JustOwner ExchangeRateRrule(InputPrice) public{
-//價格=新價格 以SZABO計算,1000000SZABO=1Ether    
-Price = (InputPrice * 1 szabo);
-}
+////////////////////////////////////////////////////////////////////////內部功能
+
+//購買721代幣內部功能
+  function NewToken(address _A,string _I,uint _V) private{
+    //總量+1
+    totalSupply++;
+    //戶口+1
+    balanceOf[_A]++;
+    //獨立網址
+    CCindex[_A] = _I;
+    //購買費用
+    ExchangeRate[_A] = _V;
+  }
+
+
+
+//轉帳時的內部功能
+    function _transfer(address _from, address _to, uint _value) internal {
+//防止轉到地址0
+    require(_to != address(0x0));
+//轉帳額少於戶口現有額
+        require(balanceOf[_from] >= _value);
+//收帳後必須大於收帳前
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+// 發送者戶口+收帳戶口額量,用於比較結果
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+// 發送者戶減少發送量
+        balanceOf[_from] -= _value;
+// 收帳戶戶堵增加發送量
+        balanceOf[_to] += _value;
+// 轉帳時通知區塊    
+        emit Transfer(_from, _to, _value);
+//轉帳完成比較previousBalances,如不同則消耗GAS但取消所有轉帳動作
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
     
 
-
-
-
-
-//修改-合約持有人 
-function ChangeAdmin(address NewOwner) 
-//新Owner不能是0
-JustOwner AddRrule(NewOwner) public{
-//Owner=新Owner
-owner = NewOwner;
 }
-
-
-
-    
-}
-
-
-
-
-
-
-
-
 
 
